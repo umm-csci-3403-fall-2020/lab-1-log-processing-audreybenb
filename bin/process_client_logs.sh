5d3cc628879cab38709b580ff9c15741969c9d83
Ben <burge337@morris.umn.edu>
2020-09-03 11:35:06 -0500

    Get the relevant text from the log files.
    Turns out the issue from before was that we were trying to pipe the results of our cd command into exit... which blew things up.
    
    We think we have all the lines that we want from the log files, but we need to get rid of the minutes and seconds on the time given in the log files.
    Our first attempt to solve this causes the program to hang up when checking whether the tests executed successfully. (syntax error?)
    
    We've also cleaned up a bit of the unnecessary bits of code (like touch failed_login_data.txt) and changed the solution structure so we first cat all the log data in a
    temporary text file, that we then scan with awk to pick up the relevant columns of information. (We'll need to remember to delete this .txt file when we are finished)

--- bin/process_client_logs.sh
+++ bin/process_client_logs.sh
@@ -2,16 +2,14 @@
 given_dir="$1"
 
 #Move to the specified directory
-cd "$given_dir" | exit
+cd "$given_dir" || exit
 
-#make the intermediate txt file
-touch failed_login_data
-
-echo $given_dir
-ls
 #Grab all of the log files in our specified directory and put them into a text file
-cat * > failed_login_data.txt 
+cat var/log/* >  all_login_data.txt
 
 #Grab only the relevant lines of text from failed_login_data.txt (invalid users and failed known users)
-#cat *.log > awk 'match($0,/([a-zA-Z0-9]+_):([A-Za-z0-9]+_): Failed password for invalid user ([a-zA-Z]+) from ([0-9].)/, groups) || match($0,/([a-zA-Z0-9]+_):([a-zA-Z0-9]+_): Failed password for ([a-zA-Z]+) from ([0-9].)/, groups) {print groups[1] groups[3] groups[4] "\n"}' > failed_login_data.txt
+#cat var/log/* | awk 'match($0,/([a-zA-Z0-9 ]):([A-Za-z0-9 ]): Failed password for invalid user ([a-zA-Z]+) from ([0-9].)/, groups) || match($0,/([a-zA-Z0-9 ]):([a-zA-Z0-9 ]): Failed password for ([a-zA-Z]+) from ([0-9].)/, groups) {print groups[1] groups[3] groups[4] "\n"}' > failed_login_data.txt
 
+#cat var/log/* | awk 'match($0,/([a-zA-Z0-9 ]):([A-Za-z0-9 ]): Failed password for invalid user ([a-zA-Z]+) from ([0-9].)/, groups) {print groups[1] groups[3] groups[4] "\n"}' > failed_login_data.txt
+
+awk '/Failed password for invalid user/ {print $1, $2, $3, $11, $13} /Failed password for ^(?!invalid)/ {print $1, $2, $3, $9, $11}' | awk 'match($0, /([a-zA-Z0-9 ]):([0-9:]) ([a-zA-Z0-9 ])/, groups) {print groups[1] groups[2]}' all_login_data.txt > failed_login_data.txt
