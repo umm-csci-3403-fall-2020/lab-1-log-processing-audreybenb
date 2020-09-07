0c313de3bb6e07b3dd3427cf0f8b5c99e71945c5
Ben <burge337@morris.umn.edu>
2020-09-06 19:20:55 -0500

    Add the changed wrap_contents.sh file to the repo.
    I had changed this while working on the lab because I found it easier to
    work with.

--- bin/wrap_contents.sh
+++ bin/wrap_contents.sh
@@ -1,3 +1,27 @@
-#!/usr/bin/env bash
-#Code from umm-csci-3403-fall-2020/pre-lab-1-audreylemeur
-cat "$2"_header.html "$1" "$2"_footer.html > "$3"
+#!/bin/bash
+#script allows for three arguments: name of the file to be wrapped, the name of the header/footer, the name
+#of the resulting file.
+
+#Variables
+#Name of starting file
+input_text=$1
+
+#Prefix of header/footer
+arg_prepend=$2
+
+#Full header file name
+header=$arg_prepend
+header+="_header.html"
+
+#Full footer file name
+footer=$arg_prepend
+footer+="_footer.html"
+
+#Name of resulting file
+output_file=$3
+
+#Create an empty resulting file with our desired name
+touch "$output_file"
+
+#Crunch all of our text together into the output file
+cat $header "$input_text" $footer > "$output_file"
