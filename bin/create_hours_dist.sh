cd21f96775b1b7abf3fa58a22e3a11e7d98c775f
Ben <burge337@morris.umn.edu>
2020-09-07 20:01:26 -0500

    Complete the create_hours_dist.sh script.
    I did this all in one shot because it is pretty much the same script as create_username_dist.sh, save
    for one or two minor changes in which columns awk prints out.
    
    I've also manually tested the graphs by wrapping them in the summary header and footer and they appear
    to be working properly.

---
+++ bin/create_hours_dist.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+#directory given in command line argument
+given_dir="$1"
+
+#saving top level dir to return to
+top_dir=$(pwd)
+
+#change to work in given directory
+cd "$given_dir" || exit 1
+
+#grab our failed_login_data.txt file in our subdirectories and print out the hours into a temporary text file
+cat ./* ./*/* | awk '{print $3}' > hours.txt
+
+#add the hours and failed logins into a temporary text file in the form: hours, failed logins
+sort hours.txt | uniq -c | awk '{print "data.addRow([\x27"$2"\x27, "$1"]);"}' > temp.txt
+
+#get rid of unneeded text file
+rm hours.txt
+
+#return to top level directory to squish our text file in between a header and footer
+cd "$top_dir" || exit 1
+
+#wrap the file
+./bin/wrap_contents.sh ./"$given_dir"/temp.txt html_components/hours_dist hours_dist.html
+
+#move the newly created html file to our command line directory
+mv hours_dist.html ./"$given_dir"
+
+#get rid of the other temp text file we made
+rm ./"$given_dir"/temp.txt
