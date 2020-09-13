557576263974193c419bc3cb75b36da9007b8345
Ben <burge337@morris.umn.edu>
2020-09-13 15:23:24 -0500

    Complete process logs Put untarred files into temp directory Process client logs on each new directory Create distributions (username, hours, country) Assemble distribution report

--- bin/process_logs.sh
+++ bin/process_logs.sh
@@ -1,10 +1,25 @@
 #!/bin/env bash
 
-temp = $(mktemp -d)
+temp_dir=$(mktemp -d)
+top_dir=$(pwd)
 
+#loop through all provided tgz files and...
 for file in "$@"
 do
-	tar -xzf file -C "$temp"  
+	#grab basename of file so we can make a directory with that name
+	base_name="$(basename "$file" .tgz)"
+	mkdir "$temp_dir"/"$base_name"
+	tar -xzf "$file" -C "$temp_dir"/"$base_name"
+	#create failed login data for each of the clients
+	./bin/process_client_logs.sh "$temp_dir"/"$base_name"
 done
-ls "$temp"
-echo "$@"
+
+./bin/create_username_dist.sh "$temp_dir"
+./bin/create_hours_dist.sh "$temp_dir"
+./bin/create_country_dist.sh "$temp_dir"
+./bin/assemble_report.sh "$temp_dir"
+
+#move the end product to our top directory so we can view it
+mv "$temp_dir"/failed_login_summary.html "$top_dir"
+rm -rf "$temp_dir"
+
