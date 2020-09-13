534e79dbe35aa69630f76871fe28c24a2b9c4872
Ben <burge337@morris.umn.edu>
2020-09-13 15:33:54 -0500

    Remove forgetten temp file

--- bin/assemble_report.sh
+++ bin/assemble_report.sh
@@ -11,3 +11,5 @@ cat *.html > "$top_dir"/temp.html
 cd "$top_dir" || exit 1
 
 ./bin/wrap_contents.sh temp.html html_components/summary_plots "$given_dir"/failed_login_summary.html 
+
+rm -r temp.html
